99eeb05e90ad3ca19eaa254c834a9754cc24ff33
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-01 19:45:14 +0530

    [sailesh] Add:Check Leap Year

---
+++ BasicCorePrograms/3_CheckLeapYear.py
@@ -0,0 +1,19 @@
+'''
+@Author:Sailesh Chauhan
+@Date:2021-06-01
+@Last Modified by:Sailesh Chauhan
+@Last Modified time:2021-06-01
+@Title:To Welcome User
+'''
+try:
+    year=int(input("Enter Year in YYYY\n"))
+    year=str(year)
+    if(len(year)==4):
+        if(int(year)%400==0):
+            print("Leap Year {0} ".format(year))
+        elif(int(year)%4==0 and int(year)%100!=0):
+            print("Leap Year {0} ".format(year))
+    else:
+        print("Enter in YYYY format")
+except:
+    print("Input should be a positive Integer")
