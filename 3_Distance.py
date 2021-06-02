a95ef64eb4c2c7b9d3d94faa4698aceef4b06625
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-02 14:08:54 +0530

    [sailesh] Add:Euclidian Distance

---
+++ 3_Distance.py
@@ -0,0 +1,23 @@
+'''
+@Author:Sailesh Chauhan
+@Date:2021-06-01
+@Last Modified by:Sailesh Chauhan
+@Last Modified time:2021-06-01
+@Title: Euclidian Distance Between origin and Cartesian Co-oridnates  
+'''
+
+import sys
+import math
+try:
+    
+    def EuclidianDistance(x,y):
+        """This Function returns Euclidian Distance"""
+        return math.sqrt(math.pow(x,2)+math.pow(y,2))
+
+    inputX_Coordinate=float(input("Enter X-coordinate"))
+    inputY_Ordinate=float(input("Enter Y-coordinate"))
+    print(EuclidianDistance(inputX_Coordinate,inputY_Ordinate))
+except Exception as ex:
+    print(ex.__class__)
+
+
