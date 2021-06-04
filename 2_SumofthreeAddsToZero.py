89cc09ab232ad1b291fcb419a3217a5c54e5f288
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-04 12:13:26 +0530

    [sailesh] Refactor:Added Doc String and userInput

--- 2_SumofthreeAddsToZero.py
+++ 2_SumofthreeAddsToZero.py
@@ -5,11 +5,22 @@
 @Last Modified time: 4:00 PM
 @Title: Sum of three integers add to zero
 '''
-import sys
-try:
-    def inputFromUser():
+
+
+def inputFromUser():
+    '''
+    Description:
+        Takes integer as input for array of integers and validate
+        user input should not be less than 1
+    Parameters:
+        No parameters
+    Return:
+        If returns a list of Integers
+    '''
+    try:
         listOfIntegers=[]
         numOfintegers=int(input("Enter number of integers to add\n"))
+        #Validation
         if(numOfintegers<0):
             print("invalid input only whole number allowed")
             quit()
@@ -17,26 +28,46 @@ try:
             value=int(input("Enter integers\n"))
             listOfIntegers.append(value)
         return listOfIntegers
-    def findThreeIntegersAddToZero(list):
-        '''Print three integers adding to Zero'''
+    except Exception as ex:
+        print(ex)
+def findThreeIntegersAddToZero(list):
+    '''
+    Description:
+        Takes list of Integers and find triplet of integers which
+        adds to zero.
+    Parameters:
+        list of integers 
+    Return:
+        This function returns flag value
+    '''
+    try:
         for firstPlace in range(len(list)-2):
             for secondPlace in range(firstPlace+1,(len(list)-1)):
                 for thirdPlace in range(secondPlace+1,len(list)):
                     if((list[firstPlace]+list[secondPlace]+list[thirdPlace])==0):
                         print(list[firstPlace],list[secondPlace],list[thirdPlace])
         return 1
- #Implemented Main definition only for practise       
-    def main():
+    except Exception as ex:
+        print(ex)
+ #Implemented Main definition     
+def main():
+    '''
+    Description:
+        It gives entry point to program. If is used to call other definition
+        in program code.
+    Parameters:
+        Do not take any parameters.
+    Return:
+        No return type.
+    '''
+    try:
+            
         flag=0
         numberList=inputFromUser()
         flag=findThreeIntegersAddToZero(numberList)
         if(flag==0):
             print("No three integers add to zero")
-    if __name__=="__main__":
-        main()
-except ValueError:
-    print("Only Integers are allowed")
-except TypeError:
-    print("Enter Valid Input")
-except Exception as ex:
-    print(ex)
+        if __name__=="__main__":
+            main()
+    except Exception as ex:
+        print(ex)
