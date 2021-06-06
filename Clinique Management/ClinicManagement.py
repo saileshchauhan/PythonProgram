cd6bae93ba2aa09896d2a651236f85aa289a2afb
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-06 23:01:21 +0530

    [sailesh] Add:Doctor class with properties

--- Clinique Management/ClinicManagement.py
+++ Clinique Management/ClinicManagement.py
@@ -15,4 +15,17 @@ FILE_PATH_JSON=config('JSON_File_Path')
 
 logging.basicConfig(filename=FILE_PATH_LOG,level=logging.CRITICAL,format='%(asctime)s - %(levelname)s - %(message)s')
 
-
+class Doctor:
+    '''
+    Description:
+        Creates Custom object with properties name,id,specialization
+        availability.
+    Properties:
+        Name,Id,Specialization,Availability
+    '''
+    def __init__(self,name,id,specialization,availability):
+        self.Name=name
+        self.Id=id
+        self.Specialization=specialization
+        self.Availability=int(availability)
+        pass
