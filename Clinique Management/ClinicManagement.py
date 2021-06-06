68afd0b3e8e90cf4f44269936d59996dce5c6de3
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-06 23:06:43 +0530

    [sailesh] Add:Method for new entry of Doctor/Patient

--- Clinique Management/ClinicManagement.py
+++ Clinique Management/ClinicManagement.py
@@ -60,3 +60,27 @@ def load_JSON_file():
             recordDictionary=json.load(file)
     except Exception as ex:
         logging.critical(ex)
+
+def add_doctor_patient_entry(KEY):
+    '''
+    Description:
+        Method add doctor and patient new entry to dictionary. Then
+        to the JSON file. 
+    Parameters:
+        Takes one parameters as KEY to recordDictionary.
+    Return:
+        None.
+    '''
+    try:
+        global recordDictionary
+        if(KEY=='DOCTOR'):
+            doctor=Doctor(input("Enter name of doctor\n"),input("Enter Id of doctor\n"),input("Enter Doctor Specialization\n"),input("Enter Doctor Availability\n"))
+            doctorRecord={doctor.Id:doctor.__dict__}
+            recordDictionary.setdefault(KEY,[]).append(doctorRecord)
+        elif(KEY=='PATIENT'):
+            patient=Patient(input("Enter name of patient\n"),input("Enter Id of patient\n"),input("Enter patient MobileNumber\n"),input("Enter patient Age\n"))
+            patientRecord={patient.Id:patient.__dict__}
+            recordDictionary.setdefault(KEY,[]).append(patientRecord)
+    except Exception as ex:
+        logging.critical(ex)
+
