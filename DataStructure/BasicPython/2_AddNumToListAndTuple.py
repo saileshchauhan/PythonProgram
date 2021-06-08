3ebe7ee17c846ed0d526e20149b50ad0edd211d4
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-08 17:32:13 +0530

    [sailesh] Add: Values in tuple and List

--- DataStructure/BasicPython/2_AddNumToListAndTuple.py
+++ DataStructure/BasicPython/2_AddNumToListAndTuple.py
@@ -3,7 +3,7 @@
 @Date:2021-06-08
 @Last Modified by:Sailesh Chauhan
 @Last Modified time:2021-06-08
-@Title:Reverse user firstName and LastName.
+@Title:Append user input to list and tuple then print.
 '''
 
 import logging,re
@@ -19,13 +19,39 @@ def validation(stringValue):
     Parameters:
         stringValue it is uerinput.
     Returns:
-        correctString after validation.
+        integer after validation.
     '''
     try:
         regexName="^[0-9]{1,}$"
         if(re.match(regexName,stringValue)):
-            return stringValue
+            return int(stringValue)
         print("Invalid Input")
         quit()
     except Exception as ex:
         logging.critical(ex)
+
+def input_to_list_tuple():
+    '''
+    Description:
+        Prints values enterd by user in list and tuples.
+    Parameters:
+        None
+    Returns:
+        None.
+    '''
+    try:
+        countOfEntry=validation(input("Enter count of list\n"))
+        listNumber=[]
+        tupleNumber=()
+        for count in range(countOfEntry):
+            userinput=validation(input("Enter whole number\n"))
+            listNumber.append(str(userinput))
+        tupleNumber=list(tupleNumber)
+        tupleNumber=listNumber+tupleNumber
+        tupleNumber=tuple(tupleNumber)
+        print("Values in list ",listNumber)
+        print("Values in Tuple",tupleNumber)
+    except Exception as ex:
+        logging.critical(ex)
+
+input_to_list_tuple()
