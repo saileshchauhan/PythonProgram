9924aa81310d4b80e74fcbf9a0dcaee7082cc8ac
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-08 21:38:38 +0530

    [sailesh] Add: print CPU count

---
+++ DataStructure/BasicPython/13_PrintNumberCPU_used.py
@@ -0,0 +1,20 @@
+'''
+@Author:Sailesh Chauhan
+@Date:2021-06-08
+@Last Modified by:Sailesh Chauhan
+@Last Modified time:2021-06-08
+@Title: Print Definition of Inbuilt Function.
+'''
+import multiprocessing
+import logging
+from decouple import config
+
+FILE_NAME=config('Log_File_Path')
+logging.basicConfig(filename=FILE_NAME,level=logging.CRITICAL,format='%(asctime)s - %(levelname)s - %(message)s')
+
+def print_cpu_used():
+    '''
+    '''
+    print(multiprocessing.cpu_count())
+
+print_cpu_used()
