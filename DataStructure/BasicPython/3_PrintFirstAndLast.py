d9b26a987485823b9e09100b122a395f677f40b9
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-08 17:56:40 +0530

    [sailesh] Add:Print list with index

---
+++ DataStructure/BasicPython/3_PrintFirstAndLast.py
@@ -0,0 +1,30 @@
+'''
+@Author:Sailesh Chauhan
+@Date:2021-06-08
+@Last Modified by:Sailesh Chauhan
+@Last Modified time:2021-06-08
+@Title:Reverse user firstName and LastName.
+'''
+
+import logging,re
+from decouple import config
+
+FILE_NAME=config('Log_File_Path')
+logging.basicConfig(filename=FILE_NAME,level=logging.CRITICAL,format='%(asctime)s - %(levelname)s - %(message)s')
+
+def print_list_index(list):
+    '''
+    Description:
+        Print values at given index in list.
+    Parameters:
+        Takes list as parameter.
+    Return:
+        None.
+    '''
+    try:
+        print(list[0])
+        print(list[len(list)-1])
+    except Exception as ex:
+        logging.critical(ex)
+
+print_list_index(["Red","Green","White","Black"])
