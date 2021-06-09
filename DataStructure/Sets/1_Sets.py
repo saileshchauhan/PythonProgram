119bbda25b23d102f07aa6dd889cbabe9f8d5ef4
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-09 20:10:26 +0530

    [sailesh] Add: Set Problems 1,2,3

--- DataStructure/Sets/1_Sets.py
+++ DataStructure/Sets/1_Sets.py
@@ -1,11 +1,35 @@
+'''
+@Author:Sailesh Chauhan
+@Date:2021-06-08
+@Last Modified by:Sailesh Chauhan
+@Last Modified time:2021-06-09
+@Title: Create set
+'''
+
 #Importing From LogConfig logging config
 import logconfig
 import logging
 
-try:
-    raise ValueError("Testing configuartion For log config")
-except Exception as ex:
-    logging.critical(ex)
+def create_set():
+    '''
+    Description:
+    Parameters:
+    Returns:
+    '''
+    try:
+        defaultSet=set()
+        choice=''
+        print("Ener any number of values to enter in array")
+        while(choice.lower()!='q'):
+            userValue=input("Enter value\n")
+            defaultSet.add(userValue)
+            print("Do you want to add more values \nPress C to continue\nQ to stop\n")
+            choice=input("Enter choice\n")
+        return set
+    except Exception as ex:
+        logging.critical(ex)
+
+print_set()
 
 
 
