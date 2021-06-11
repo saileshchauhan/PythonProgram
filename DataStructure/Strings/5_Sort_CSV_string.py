f9b8b168b6a346f5e35899b913db83baa361d4ae
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-11 17:01:38 +0530

    [sailesh] Add:CSV string

--- DataStructure/Strings/5_Sort_CSV_string.py
+++ DataStructure/Strings/5_Sort_CSV_string.py
@@ -3,11 +3,10 @@
 @Date:2021-06-11
 @Last Modified by:Sailesh Chauhan
 @Last Modified time:2021-06-11
-@Title: Print Input from user in uppercase and lower.
+@Title: Print CSV String after sorting to CSV.
 '''
 
 #Importing logConfig for error logging
-
 import logconfig
 import logging,re
 
@@ -36,8 +35,24 @@ def validation(stringValue):
         regexName="^[a-zA-Z0-9,.]{3,}[a-zA-Z]+$"
         if(re.match(regexName,stringValue)):
             return stringValue
-        print("String Lenght must be more than 3")
+        logging.debug("String Lenght must be more than 3")
     except Exception as ex:
         logging.critical(ex)
 
-print(create_string())
+def csv_sorting(string):
+    '''
+    Description:
+    Parameters:
+    Returns:
+    '''
+    try:
+        csvList=string.split(',')
+        csvList.sort()
+        logging.debug(list(set(csvList)))
+    except Exception as ex:
+        logging.critical(ex)
+
+csvString=create_string()
+csv_sorting(csvString)
+
+
