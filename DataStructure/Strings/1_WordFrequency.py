abac33ee1b5759f4ebf3cb0055ce4af593df78c1
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-11 09:11:44 +0530

    [sailesh] Add:char frequency and replacement

---
+++ DataStructure/Strings/1_WordFrequency.py
@@ -0,0 +1,55 @@
+'''
+@Author:Sailesh Chauhan
+@Date:2021-06-11
+@Last Modified by:Sailesh Chauhan
+@Last Modified time:2021-06-11
+@Title: Get length of string count each alphabet frequency and char replacement.
+'''
+
+#Importing logConfig for error logging
+import logconfig
+import logging,re
+
+def create_list():
+    '''
+    Description:
+    Parameters:
+    Returns:
+    '''
+    try:
+        userValue=validation(input("You can enter any word"))
+        return userValue   
+    except Exception as ex:
+        logging.error(ex)
+
+def validation(stringValue):
+    '''
+    Description:
+        Provides validation for userinput.
+    Parameters:
+        stringValue it is userinput.
+    Returns:
+        correctString after validation.
+    '''
+    try:
+        regexName="^[a-zA-Z0-9]{3,}$"
+        if(re.match(regexName,stringValue)):
+            return stringValue
+        print("String Lenght must be more than 3")
+    except Exception as ex:
+        logging.critical(ex)
+
+def char_frequency(word):
+    '''
+    Description:
+    Parameters:
+    Returns:
+    '''
+    try:
+        count=0
+        charFrequency={}
+        for char in word:
+            for index in range(len(word)):
+                if(char==word[index]):
+                    count+=1
+            charFrequency.update({char:count})
