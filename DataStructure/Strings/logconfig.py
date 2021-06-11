806fddb345536e00b016352d56bd1c603d0c351b
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-11 18:00:25 +0530

    [sailesh] Refactor:Removed logconfig

--- DataStructure/Strings/logconfig.py
+++
@@ -1,5 +0,0 @@
-import logging
-from decouple import config
-
-FILE_NAME=config('Log_File_Path')
-logging.basicConfig(filename=FILE_NAME,level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s')
