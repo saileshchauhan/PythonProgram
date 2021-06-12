6901c10042170e508856642e4783805a94b7532e
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-12 10:07:28 +0530

    [sailesh] Add: Count True value

---
+++ DataStructure/Dictionary/6_CountVal_NestedDict.py
@@ -0,0 +1,46 @@
+'''
+@Author:Sailesh Chauhan
+@Date:2021-06-12
+@Last Modified by:Sailesh Chauhan
+@Last Modified time:2021-06-12
+@Title: Count values associated with key in dictionary.
+'''
+
+#Importing logConfig for error logging
+import logconfig
+import logging
+
+def count_values(listDictionary):
+    '''
+    Description:
+    Parameters:
+    Returns:
+    '''
+    try:
+        count=0
+        for item in listDictionary:
+            if True in item.values():
+                count+=1
+        return count
+    except Exception as ex:
+        logging.critical(ex)
+
+countTrue=count_values([{'id': 1, 'success': True, 'name': 'Lary'},{'id': 2, 'success':False, 'name': 'Rabi'},{'id': 3, 'success': True, 'name': 'Alex'}])
+print(countTrue)
+
+#Solving same problem using List Comprehension.
+
+def count_values_listComprehension(listDictionary):
+    '''
+    Description:
+    Parameters:
+    Returns:
+    '''
+    try:
+        newList=[x for item in listDictionary for x in item.values() if x==True]
+        return newList
+    except Exception as ex:
+        logging.critical(ex)
+
+list=count_values_listComprehension([{'id': 1, 'success': True, 'name': 'Lary'},{'id': 2, 'success':False, 'name': 'Rabi'},{'id': 3, 'success': True, 'name': 'Alex'}])
+print(list)
