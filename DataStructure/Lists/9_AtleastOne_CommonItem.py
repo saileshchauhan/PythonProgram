66cde20d57e0c40b3a3da0e596f139de296dd21c
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-12 08:39:49 +0530

    [sailesh] Add:Covert word to Dict

--- DataStructure/Lists/9_AtleastOne_CommonItem.py
+++ DataStructure/Lists/9_AtleastOne_CommonItem.py
@@ -43,7 +43,7 @@ def validation(stringValue):
         regexName="^[0-9a-zA-Z]{1,}$"
         if(re.match(regexName,stringValue)):
             return (stringValue)
-        print("Invalid Input")
+        logging.debug("Invalid Input")
     except Exception as ex:
         logging.critical(ex)
 
@@ -60,7 +60,7 @@ def atleast_one_common(list1,list2):
         for item in list1:
             for element in list2:
                 if(item==element):
-                    print(item)
+                    logging.debug(item)
                     return True
     except Exception as ex:
         logging.critical(ex)
