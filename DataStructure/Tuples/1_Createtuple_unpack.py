3796d62e6468a8e49baf79b854f8b0a75645707e
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-12 16:12:15 +0530

    [sailesh] Add:Tuples

--- DataStructure/Tuples/1_Createtuple_unpack.py
+++ DataStructure/Tuples/1_Createtuple_unpack.py
@@ -1,45 +1,30 @@
 '''
 @Author:Sailesh Chauhan
-@Date:2021-06-08
+@Date:2021-06-12
 @Last Modified by:Sailesh Chauhan
-@Last Modified time:2021-06-09
-@Title: Create set Add Element Iterate through set element.
+@Last Modified time:2021-06-12
+@Title: Convert list to tuple 
 '''
 
 #Importing logConfig for error logging
 import logconfig
 import logging
 
-def create_set():
+def create_list():
     '''
     Description:
     Parameters:
     Returns:
     '''
     try:
-        defaultSet=set()
+        defaultList=list()
         choice=''
         print("You can enter any value in set")
         while(choice.lower()!='q'):
             userValue=input("Enter value to add in set\n")
-            defaultSet.add(userValue)
+            defaultList.append(userValue)
             print("Do you want to add more values \nPress C to continue\nQ to stop\n")
             choice=input("Enter choice\n")
-        return defaultSet
+        return defaultList
     except Exception as ex:
         logging.error(ex)
-
-
-def iterate_set(set=create_set()):
-    '''
-    Description:
-    Parameters:
-    Returns:
-    '''
-    try:
-        for eachSetItem in set:
-            logging.debug(eachSetItem)
-    except Exception as ex:
-        logging.error(ex)
-
-iterate_set()
