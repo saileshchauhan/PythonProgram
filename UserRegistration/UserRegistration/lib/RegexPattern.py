79f70cf1f01cdf32d5c6f513c302d0d9d2ed9a8b
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-13 14:25:43 +0530

    [sailesh] Add:RegexPattern for first Name

--- UserRegistration/UserRegistration/lib/RegexPattern.py
+++ UserRegistration/UserRegistration/lib/RegexPattern.py
@@ -3,5 +3,22 @@
 @Date:2021-06-13
 @Last Modified by:Sailesh Chauhan
 @Last Modified time:2021-06-13
-@Title: Convert list to tuple and unpack the tuple in variable and list.
+@Title: User Registration.
 '''
+
+from UserRegistartion import logConfig
+import re
+
+class RegexPattern:
+
+    REGEX_PASSWORD = "^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$"
+    REGEX_MOBILE="^[+][0-9]{1,3}[ ][1-9]{1}[0-9]{9}$"
+    REGEX_EMAIL = "^[a-z0-9A-Z]+([./*$#%][a-zA-Z0-9]{1,})?[@][a-zA-Z]{2,}[.][a-zA-Z]{2,}([.][a-zA-Z]{2})?$"
+    REGEX_LASTNAME = "^[A-Z]{1}[a-z]{2,}$"
+    
+    REGEX_PINCODE = "^[1-9]{1}[0-9]{2,2}[ ]?[0-9]{3}$"
+
+    def ValidateFirstName(self,firstName,REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{2,}$"):
+        return re.IsMatch(firstName, REGEX_FIRSTNAME)
+    pass
+
