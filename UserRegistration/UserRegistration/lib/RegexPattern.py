ac4705abb9b224d971babce967a0acf8270ac8ed
saileshchauhan <chauhansailesh68@gmail.com>
2021-06-13 14:04:45 +0530

    [sailesh] Add:Virtual env setup

---
+++ UserRegistration/UserRegistration/lib/RegexPattern.py
@@ -0,0 +1,7 @@
+'''
+@Author:Sailesh Chauhan
+@Date:2021-06-13
+@Last Modified by:Sailesh Chauhan
+@Last Modified time:2021-06-13
+@Title: Convert list to tuple and unpack the tuple in variable and list.
+'''
